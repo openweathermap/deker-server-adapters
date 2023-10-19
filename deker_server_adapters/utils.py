1e937333181fe299dca6ee496a317a1051a7f9e8
gurich92 <gurich06@gmail.com>
2023-10-19 13:00:31 +0100

    Add fast way to making request in case there is only one node

--- deker_server_adapters/utils.py
+++ deker_server_adapters/utils.py
@@ -8,14 +8,13 @@ from httpx import Client, Response
 
 from deker_server_adapters.consts import STATUS_OK
 
-
 logger = getLogger(__name__)
 
 
 def request_in_cluster(
-    url: str, nodes: Sequence, client: Client, request_kwargs: Optional[Dict] = None
+        url: str, nodes: Sequence, client: Client, request_kwargs: Optional[Dict] = None
 ) -> Optional[Response]:
-    """Make GET request on random node, while response is not recieved.
+    """Make GET request on random node, while response is not received.
 
     :param url: What we request
     :param request_kwargs: Kwargs for request
@@ -24,18 +23,46 @@ def request_in_cluster(
     """
     response = None
     nodes = list(nodes)
-    while nodes and (response is None or response.status_code != STATUS_OK):
-        index = randint(0, len(nodes) - 1)
-        node = nodes.pop(index)
 
-        try:
-            if request_kwargs:
-                response = client.get(f"{node}/{url}", **request_kwargs)
-            else:
-                response = client.get(f"{node}/{url}")
-        except Exception:
-            logger.error(f"Coudn't get response from {node}")  # noqa
-            traceback.print_exc()
-            continue
+    if len(nodes) == 0:
+        return response
+
+    if len(nodes) == 1:
+        node = nodes.pop(0)
+        response = _request(url, node, client, request_kwargs)
+    else:
+        while nodes and (response is None or response.status_code != STATUS_OK):
+            index = randint(0, len(nodes) - 1)
+            node = nodes.pop(index)
+
+            response = _request(url, node, client, request_kwargs)
+
+            if response is None:
+                continue
+
     print(response)
+    return response
+
+
+def _request(
+        url: str, node: str, client: Client, request_kwargs: Optional[Dict] = None
+) -> Optional[Response]:
+    """Internal request func - Make GET request on given node.
+
+    :param url: What we request
+    :param request_kwargs: Kwargs for request
+    :param node: Node for requesting
+    :param client: Httpx Client
+    """
+    response = None
+
+    try:
+        if request_kwargs:
+            response = client.get(f"{node}/{url}", **request_kwargs)
+        else:
+            response = client.get(f"{node}/{url}")
+    except Exception:
+        logger.error(f"Coudn't get response from {node}")  # noqa
+        traceback.print_exc()
+
     return response
