f4bc581b2bb470fed11e3bfe6f4c15da79ae219f
matveyvarg <matveyvarg@gmail.com>
2023-10-30 15:52:41 +0100

    - strip of url

--- deker_server_adapters/utils.py
+++ deker_server_adapters/utils.py
@@ -23,9 +23,9 @@ def _request(url: str, node: str, client: Client, request_kwargs: Optional[Dict]
 
     try:
         if request_kwargs:
-            response = client.get(f"{node.rstrip('/')}/{url}", **request_kwargs)
+            response = client.get(f"{node.rstrip('/')}/{url.lstrip('/')}", **request_kwargs)
         else:
-            response = client.get(f"{node.rstrip('/')}/{url}")
+            response = client.get(f"{node.rstrip('/')}/{url.lstrip('/')}")
     except Exception:
         logger.exception(f"Coudn't get response from {node}")  # noqa
 
